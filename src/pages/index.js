59a29206b5180a4336a006ca26ee6a3617d1bf06
Ricardo Medina <rcrdmedina6@gmail.com>
2019-11-30 20:44:04 -0400

    Imagenes responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,10 +21,6 @@ const IndexPage = ({ data }) => (
   <>
     <SEO title="Home" />
     <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/gracias/">Go to gracias</Link>
   </>
 )
 
